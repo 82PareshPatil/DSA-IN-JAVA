484eba322f2375f5bbb66d731a8fa78119e5e718
Paresh Patil <paresh2004patil@gmail.com>
2024-11-11 12:50:57 +0530

    new java code added

--- arrycreation.java
+++ arrycreation.java
@@ -1,7 +1,19 @@
 import java.util.*;
+import java.lang.*;
 public class arrycreation
 {
     public static void main(String[] args) {
-        System.err.println("Hello");
+         int a[]=new int[10];
+         Scanner s=new Scanner(System.in);
+         for(int i=0;i<10;i++)
+         {
+            System.out.print("Enter A number");
+            a[i]=s.nextInt();
+         }
+         for(int i=0;i<10;i++)
+         {
+           System.out.println((i+1)+":"+a[i]);                  
+         }
+        
     }
 }
